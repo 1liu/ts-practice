f2ba89a8932ee53e84a2b014eb45dbcbb0ea03ea
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-08-18 12:08:09 -0700

    ts asserts

---
+++ 06/asserts.ts
@@ -0,0 +1,9 @@
+var num:number|string = 1;
+num = 'aaa';
+
+console.log(num.length)
+
+function getAssert(num:number|string){
+  //return (<string>num).length;
+  return (name as string).length; //in jsx
+}
